a84bb5bab1bad19fd954ffdfb32deeaaf7a1e7ee
opeodedeyi <opeyemiodedeyi@gmail.com>
2020-07-05 04:55:00 +0100

    Initial commit

---
+++ js/nav.js
@@ -0,0 +1,23 @@
+var toggleButton = document.querySelector('.openNav');
+var mobileNav = document.querySelector('.mobile-navigation');
+var backdrop = document.querySelector('.backdrop');
+var closeButton = document.querySelector('.close-mobile-nav');
+
+
+toggleButton.addEventListener('click', function() {
+    console.log('clicked toggle')
+    mobileNav.style.display = 'flex';
+    backdrop.style.display = 'block';
+})
+
+backdrop.addEventListener('click', function() {
+    console.log('clicked backdrop')
+    mobileNav.style.display = 'none';
+    backdrop.style.display = 'none';
+})
+
+closeButton.addEventListener('click', function() {
+    console.log('clicked cancle button')
+    mobileNav.style.display = 'none';
+    backdrop.style.display = 'none';
+})
